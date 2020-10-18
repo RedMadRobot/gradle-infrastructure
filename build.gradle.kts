364832da33701893b1a243407c3bdba8d77f4e29
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2020-10-18 22:10:02 +0300

    version: 0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "com.redmadrobot.build"
 description = "Small plugins to reduce boilerplate in Gradle build scripts."
-version = "0.2-SNAPSHOT"
+version = "0.2"
 
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
