62fc16666932f5ee64c368932902877d75c32d5c
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2020-11-29 16:28:58 +0300

    android-library: Look at kotlin.explicitApi for android libraries

--- samples/android-application-multi/module2/build.gradle.kts
+++ samples/android-application-multi/module2/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("redmadrobot.android-library")
 }
 
+// Explicit API is enabled by default, but we can disable it if need
+kotlin.explicitApi = null
+
 dependencies {
     // Kotlin already added as dependency to the project
     // ...
--- src/main/kotlin/AndroidLibraryPlugin.kt
+++ src/main/kotlin/AndroidLibraryPlugin.kt
@@ -3,15 +3,15 @@ package com.redmadrobot.build
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+private const val ARG_EXPLICIT_API = "-Xexplicit-api"
 
 class AndroidLibraryPlugin : BaseAndroidPlugin() {
     override fun apply(target: Project) = with(target) {
         apply(plugin = "com.android.library")
         super.apply(target)
-
-        kotlinCompile {
-            kotlinOptions.freeCompilerArgs += "-Xexplicit-api=strict"
-        }
 
         android<LibraryExtension> {
             buildFeatures {
@@ -23,5 +23,23 @@ class AndroidLibraryPlugin : BaseAndroidPlugin() {
         }
 
         configureKotlinDependencies()
+
+        // Enable Explicit API by default
+        if (kotlin.explicitApi == null) kotlin.explicitApi()
+        afterEvaluate {
+            configureExplicitApi(kotlin.explicitApi)
+        }
     }
+}
+
+private fun Project.configureExplicitApi(mode: ExplicitApiMode?) {
+    if (mode == null) return
+
+    tasks.matching { it is KotlinCompile && !it.name.contains("test", ignoreCase = true) }
+        .configureEach {
+            val options = (this as KotlinCompile).kotlinOptions
+            if (options.freeCompilerArgs.none { it.startsWith(ARG_EXPLICIT_API) }) {
+                options.freeCompilerArgs += mode.toCompilerArg()
+            }
+        }
 }
