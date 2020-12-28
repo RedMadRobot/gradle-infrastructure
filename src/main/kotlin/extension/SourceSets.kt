cbc8e3ed37720aba56cb06642d87894b476ffbbe
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2020-12-28 20:19:40 +0300

    android: Create addSharedSourceSetRoot method

--- src/main/kotlin/extension/SourceSets.kt
+++ src/main/kotlin/extension/SourceSets.kt
@@ -1,9 +1,12 @@
 package com.redmadrobot.build.extension
 
+import com.android.SdkConstants.*
+import com.android.build.api.dsl.AndroidSourceSet
 import com.android.build.gradle.BaseExtension
 import com.redmadrobot.build.BUILD_TYPE_DEBUG
 import com.redmadrobot.build.BUILD_TYPE_RELEASE
 import com.redmadrobot.build.BUILD_TYPE_STAGING
+import org.gradle.api.Incubating
 import org.gradle.kotlin.dsl.get
 
 /**
@@ -37,4 +40,37 @@ public fun BaseExtension.mergeSourceSets(from: String, to: String) {
     val fromSourceSet = sourceSets[from].java
     val toSourceSet = sourceSets[to].java
     toSourceSet.setSrcDirs(fromSourceSet.srcDirs + toSourceSet.srcDirs)
+}
+
+/**
+ * Adds source set root with given [name] shared between [variant1] and [variant2] source sets.
+ * By default [name] is a combination of `variant1` and `variant2` names.
+ */
+@Incubating
+public fun BaseExtension.addSharedSourceSetRoot(
+    variant1: String,
+    variant2: String,
+    name: String = "$variant1${variant2.capitalize()}"
+) {
+    val variant1SourceSets = sourceSets[variant1]
+    val variant2SourceSets = sourceSets[variant2]
+    val root = "src/$name"
+
+    variant1SourceSets.addRoot(root)
+    variant2SourceSets.addRoot(root)
+}
+
+@Suppress("UnstableApiUsage")
+private fun AndroidSourceSet.addRoot(path: String) {
+    java.setSrcDirs(listOf("$path/$FD_JAVA"))
+    resources.setSrcDirs(listOf("$path/$FD_JAVA_RES"))
+    res.setSrcDirs(listOf("$path/$FD_RES"))
+    assets.setSrcDirs(listOf("$path/$FD_ASSETS"))
+    manifest.srcFile("$path/$FN_ANDROID_MANIFEST_XML")
+    aidl.setSrcDirs(listOf("$path/$FD_AIDL"))
+    renderscript.setSrcDirs(listOf("$path/$FD_RENDERSCRIPT"))
+    jni.setSrcDirs(listOf("$path/$FD_JNI"))
+    jniLibs.setSrcDirs(listOf("$path/jniLibs"))
+    shaders.setSrcDirs(listOf("$path/shaders"))
+    mlModels.setSrcDirs(listOf("$path/$FD_ML_MODELS"))
 }
