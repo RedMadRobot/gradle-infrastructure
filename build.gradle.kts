1820369ea3e452c4e1a88f16e55994ff08279629
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-02-25 20:37:16 +0300

    update: infrastructure 0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,28 @@
+import com.redmadrobot.build.extension.developer
+import com.redmadrobot.build.extension.mit
+import com.redmadrobot.build.extension.setGitHubProject
+
 plugins {
-    id("redmadrobot.root-project") version "0.6"
+    id("redmadrobot.root-project") version "0.8"
     id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 apply(plugin = "redmadrobot.detekt")
+
+redmadrobot {
+    publishing {
+        signArtifacts = true
+
+        pom {
+            setGitHubProject("RedMadRobot/gradle-infrastructure")
+
+            licenses {
+                mit()
+            }
+
+            developers {
+                developer(id = "osipxd", name = "Osip Fatkullin", email = "o.fatkullin@redmadrobot.com")
+            }
+        }
+    }
+}
