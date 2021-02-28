d0d4e74f578c874d2f8ef7752e9c0616bcf09445
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-02-28 15:29:31 +0300

    update: infrastructure 0.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,11 @@ import com.redmadrobot.build.extension.mit
 import com.redmadrobot.build.extension.setGitHubProject
 
 plugins {
-    id("redmadrobot.root-project") version "0.8"
+    id("redmadrobot.root-project") version "0.8.1"
     id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 apply(plugin = "redmadrobot.detekt")
-
-repositories {
-    jcenter() // TODO #36: Remove JCenter when detekt-formatting will be moved to Maven Central
-}
 
 redmadrobot {
     publishing {
