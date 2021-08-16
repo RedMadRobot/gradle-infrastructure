58020bfe33ecd2894cc8940582dceff75944a0ae
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-08-16 15:54:15 +0300

    samples: Update dependencies

--- samples/android-application/app/build.gradle.kts
+++ samples/android-application/app/build.gradle.kts
@@ -27,14 +27,14 @@ android {
 
 dependencies {
     // Algin Kotlin version across all dependencies
-    implementation(platform(kotlin("bom", version = "1.5.20")))
+    implementation(platform(kotlin("bom", version = "1.5.21")))
 
     // Kotlin components can be added without version specifying
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
