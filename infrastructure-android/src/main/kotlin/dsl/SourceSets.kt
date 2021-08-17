aa1b0864e14e15142e78c416d432f10f70ace25e
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-08-17 10:15:20 +0300

    android: Update AGP to 7.0.0

--- infrastructure-android/src/main/kotlin/dsl/SourceSets.kt
+++ infrastructure-android/src/main/kotlin/dsl/SourceSets.kt
@@ -1,6 +1,5 @@
 package com.redmadrobot.build.dsl
 
-import com.android.SdkConstants.*
 import com.android.build.api.dsl.AndroidSourceSet
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.Incubating
@@ -33,17 +32,18 @@ public fun BaseExtension.addSharedSourceSetRoot(
     variant2SourceSets.addRoot(root)
 }
 
+// See: DefaultAndroidSourceSet.initRoot
 @Suppress("UnstableApiUsage")
 private fun AndroidSourceSet.addRoot(path: String) {
-    java.srcDirs("$path/$FD_JAVA", "$path/kotlin")
-    resources.srcDir("$path/$FD_JAVA_RES")
-    res.srcDir("$path/$FD_RES")
-    assets.srcDir("$path/$FD_ASSETS")
-    manifest.srcFile("$path/$FN_ANDROID_MANIFEST_XML")
-    aidl.srcDir("$path/$FD_AIDL")
-    renderscript.srcDir("$path/$FD_RENDERSCRIPT")
-    jni.srcDir("$path/$FD_JNI")
+    java.srcDir("$path/java")
+    kotlin.srcDirs("$path/java", "$path/kotlin")
+    resources.srcDir("$path/resources")
+    res.srcDir("$path/res")
+    assets.srcDir("$path/assets")
+    manifest.srcFile("$path/AndroidManifest.xml")
+    aidl.srcDir("$path/aidl")
+    renderscript.srcDir("$path/rs")
     jniLibs.srcDir("$path/jniLibs")
     shaders.srcDir("$path/shaders")
-    mlModels.srcDir("$path/$FD_ML_MODELS")
+    mlModels.srcDir("$path/ml")
 }
--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 
 dependencies {
     api(kotlin("gradle-plugin", version = "1.5.20"))
-    compileOnly("com.android.tools.build:gradle:4.2.1")
+    compileOnly("com.android.tools.build:gradle:7.0.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.17.1")
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.12.0.202106070339-r")
 }
