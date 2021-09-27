206b8a7971ac1ad053d480da1a417b8611cc939e
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2021-09-27 10:47:17 +0300

    samples: Update dependencies

--- samples/android-application-multi/app/build.gradle.kts
+++ samples/android-application-multi/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
 dependencies {
     // Align Kotlin version across all dependencies
-    implementation(platform(kotlin("bom", version = "1.5.30")))
+    implementation(platform(kotlin("bom", version = "1.5.31")))
 
     // Kotlin components can be added without version specifying
     implementation(kotlin("stdlib-jdk8"))
@@ -27,12 +27,12 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     testImplementation(kotlin("test-junit5"))
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
 }
