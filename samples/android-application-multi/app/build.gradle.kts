55557cdcc94c828b594d4166b24935c212b17efa
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2022-01-14 18:09:07 +0300

    samples: Update dependencies

--- samples/android-application-multi/app/build.gradle.kts
+++ samples/android-application-multi/app/build.gradle.kts
@@ -25,14 +25,14 @@ dependencies {
     implementation(project(":module1"))
     implementation(project(":module2"))
 
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     testImplementation(kotlin("test-junit5"))
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
