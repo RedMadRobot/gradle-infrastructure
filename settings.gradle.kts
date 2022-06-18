b314aafdfe797a66b7749befcf4332bca7c00248
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2022-06-18 16:22:16 +0300

    refactor: Remove deprecated module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ enableFeaturePreview("VERSION_CATALOGS")
 rootProject.name = "gradle-infrastructure"
 
 include(
-    ":infrastructure",
     ":infrastructure-android",
     ":infrastructure-kotlin",
     ":infrastructure-detekt",
