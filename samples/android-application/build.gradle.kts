a356e01e78c657ac6efb9fc8a77cf3f2521b3f5e
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2024-07-08 17:59:15 +0200

    samples: Update dependencies

--- samples/android-application/build.gradle.kts
+++ samples/android-application/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Specify needed versions of AGP and KGP
-    id("com.android.application") version "7.4.2" apply false
-    kotlin("android") version "1.8.10" apply false
+    id("com.android.application") version "8.5.0" apply false
+    kotlin("android") version "2.0.0" apply false
 
     id("com.redmadrobot.android-config") version "0.19-SNAPSHOT"
 }
