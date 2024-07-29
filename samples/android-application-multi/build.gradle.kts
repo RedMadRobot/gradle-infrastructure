67ff2c6e3cbc2c897d99c671fc6c379f8bae25bd
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2024-07-29 11:04:28 +0200

    version: 0.19.1-SNAPSHOT

--- samples/android-application-multi/build.gradle.kts
+++ samples/android-application-multi/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Versions of AGP and KGP are specified in buildSrc module
-    id("com.redmadrobot.android-config") version "0.19"
+    id("com.redmadrobot.android-config") version "0.19.1-SNAPSHOT"
 }
 
 // Common configurations for all modules
