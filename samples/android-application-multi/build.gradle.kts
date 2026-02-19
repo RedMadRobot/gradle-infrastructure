ab5806ee1f6d8909616df759318d68ad4f686874
a.tabolin <a.tabolin@redmadrobot.com>
2026-02-19 15:07:54 +0300

    version: 0.20.0-SNAPSHOT

--- samples/android-application-multi/build.gradle.kts
+++ samples/android-application-multi/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Versions of AGP and KGP are specified in buildSrc module
-    id("com.redmadrobot.android-config") version "0.20-SNAPSHOT"
+    id("com.redmadrobot.android-config") version "0.20.0-SNAPSHOT"
 }
 
 // Common configurations for all modules
